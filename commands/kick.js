5352a01987812c0b3e098edbc57bf175a13d5577
Krchip <35456778+Krchip@users.noreply.github.com>
2020-11-24 16:43:13 +0900

    정의된 변수 중복

--- commands/kick.js
+++ commands/kick.js
@@ -5,11 +5,9 @@ module.exports = {
     description: "추방 커맨드",
     excute(client, message) {
         if (!message.member.hasPermission("KICK_MEMBERS")) return error.denyofuse(message, "KICK_MEMBERS")
-        let args = message.mentions.members.first()
-
-        if (!args) return error.invaildCommand(message, "경손아 추방 < 멘션 >")
-
         let member = message.mentions.members.first()
+
+        if (!member) return error.invaildCommand(message, "경손아 추방 < 멘션 >")
 
         member
             .kick()
